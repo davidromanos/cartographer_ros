f084ded572bfe828cf1409195bda1ff934214753
Himal Kooverjee <hkooverjee@gmail.com>
2018-03-29 16:20:34 +0200

    adjusting configuration files and others

--- cartographer_ros/configuration_files/aaucart_2d_IMU.lua
+++ cartographer_ros/configuration_files/aaucart_2d_IMU.lua
@@ -19,7 +19,7 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
+  tracking_frame = "imu_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
@@ -27,13 +27,13 @@ options = {
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 2, 
+  num_laser_scans = 1, 
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 0.1,
+  pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
@@ -43,18 +43,19 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+-- TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 10
+
 
 -- TRAJECTORY_BUILDER_2D = {
---   use_imu_data = false,
-  -- min_range = 0.5.,
-  -- max_range = 8.,
-  -- min_z = -0.8,
-  -- max_z = 2.,
-  -- missing_data_ray_length = 5.,
-  -- num_accumulated_range_data = 50,
-  -- voxel_filter_size = 0.025,
+--   use_imu_data = true,
+--   min_range = 0.,
+--   max_range = 30.,
+--   min_z = -0.8,
+--   max_z = 2.,
+--   missing_data_ray_length = 5.,
+--   num_accumulated_range_data = 50,
+--   voxel_filter_size = 0.025,
 
 --   adaptive_voxel_filter = {
 --     max_length = 0.5,
@@ -65,10 +66,10 @@ TRAJECTORY_BUILDER_2D.use_imu_data = false
 --   loop_closure_adaptive_voxel_filter = {
 --     max_length = 0.9,
 --     min_num_points = 100,
---     max_range = 10.,
+--     max_range = 50.,
 --   },
 
---   use_online_correlative_scan_matching = true,
+--   use_online_correlative_scan_matching = false,
 --   real_time_correlative_scan_matcher = {
 --     linear_search_window = 0.1,
 --     angular_search_window = math.rad(20.),
